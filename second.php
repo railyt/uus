5c1410974819b630ba867979fa45a7cbde9807c2
railtoom <railtoom@greeny.cs.tlu.ee>
2016-09-14 14:49:38 +0300

    paljuonne

---
+++ second.php
@@ -0,0 +1,31 @@
+<?php
+
+	$age=5;
+	if ($age<18)
+	{echo "alaealine";
+		}else{
+			echo "täisealine";
+			}
+
+
+?>
+
+<br>
+
+<?php
+
+	//saab märkmeid teha
+	
+	for($i=0; $i<=$age; $i=$i+1) {
+		
+		echo $i. "palju ";
+	}
+
+	echo "õnne";
+
+
+	
+	
+	
+
+?>
